eefc65f595d7af8f2659f38bd66adbf816e70308
dev-garcia <garcia.ac.cristian@gmail.com>
2025-07-20 16:36:17 -0400

    chore: install and configure swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('FPF - Planning Poker')
+    .setDescription(
+      'The FPF - Planning Poker API description by Cristian Garcia',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `Swagger disponível em http://localhost:${process.env.PORT ?? 3000}/docs`,
+  );
 }
 bootstrap();
